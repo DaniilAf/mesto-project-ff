397d2e5f1c647c3aa17cc3e59fa5a495483884da
DaniilAf <danya.afinogenov@yandex.ru>
2024-09-17 19:24:12 +0300

    первая итерация

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function addCard(element, delCard){
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+  cardElement.querySelector('.card__image').src = element.link
+  cardElement.querySelector('.card__image').alt = element.name
+  cardElement.querySelector('.card__title').textContent = element.name
 
-// @todo: Функция удаления карточки
+  const deleteCardButton = cardElement.querySelector ('.card__delete-button');
+  deleteCardButton.addEventListener('click', () => {
+    delCard(cardElement);
+  });
 
-// @todo: Вывести карточки на страницу
+  return cardElement; 
+  };
+
+// // @todo: Функция удаления карточки
+function removeCard(card) {
+  card.remove();
+};
+
+// // @todo: Вывести карточки на страницу
+initialCards.forEach(function(elem) {
+  const card = addCard(elem, removeCard);
+  placesList.append(card);
+});
