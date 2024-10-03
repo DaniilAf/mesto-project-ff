2cab525f21a4c45441ef89015f38471eefaf6b50
DaniilAf <danya.afinogenov@yandex.ru>
2024-10-03 22:24:38 +0300

    модальные открываются

--- src/index.js
+++ src/index.js
@@ -1,22 +1,7 @@
-import addIcon from './images/add-icon.svg';
-import close from './images/close.svg';
-import deleteIcon from './images/delete-icon.svg';
-import editIcon from './images/edit-icon.svg';
-import likeActive from './images/like-active.svg';
-import likeInactive from './images/like-inactive.svg';
-
-
-const allIconCards = [
-  { name: 'add icon', link: addIcon },
-  { name: 'close', link: close },
-  { name: 'delete icon', link: deleteIcon },
-  { name: 'edit icon', link: editIcon },
-  { name: 'like active', link: likeActive },
-  { name: 'like inactive', link: likeInactive },
-]; 
-
 import './pages/index.css';
 import { initialCards } from './scripts/cards.js';
+
+import { openPopup } from './scripts/modal.js';
 
 // @todo: Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
@@ -27,13 +12,24 @@ const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
 function createCard(element, delCard){
   const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
-  cardElement.querySelector('.card__image').src = element.link
-  cardElement.querySelector('.card__image').alt = element.name
-  cardElement.querySelector('.card__title').textContent = element.name
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+
+  cardImage.src = element.link;
+  cardImage.alt = element.name;
+  cardTitle.textContent = element.name;
+
+  cardElement.querySelector('.card__like-button').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('card__like-button_is-active')
+  });
 
   const deleteCardButton = cardElement.querySelector ('.card__delete-button');
   deleteCardButton.addEventListener('click', () => {
     delCard(cardElement);
+  });
+
+  cardImage.addEventListener('click', () => {
+    openPopup(popupTypeImage)
   });
 
   return cardElement; 
@@ -51,25 +47,26 @@ initialCards.forEach(function(elem) {
 });
 
 
-// @todo: Открытие и закрытие popup
-const popup = document.querySelector('.popup');
-const popupCloseButton = popup.querySelector('.popup__close');
-const profileEditButton = document.querySelector('.profile__edit-button');
-const profileAddButton = document.querySelector('.profile__add-button');
-// const cardMage = cardTemplate.querySelector('.card__image');
 
-profileEditButton.addEventListener('click', function(){
-  popup.classList.add('popup_is-opened');
-});
+// Кнопки вызова POPUP
+const page = document.querySelector('.page');
+const buttonProfileEdit = page.querySelector('.profile__edit-button');
+const profileAddButton = page.querySelector('.profile__add-button');
 
-profileAddButton.addEventListener('click', function(){
-  popup.classList.add('popup_is-opened');
-});
+// POPUP
+const popupTypeImage = page.querySelector('.popup_type_image');
+const popupTypeEdit = page.querySelector('.popup_type_edit');
+const popupTypeNewCard = page.querySelector('.popup_type_new-card');
 
-// cardMage.addEventListener('click', function(){
-//   popup.classList.add('popup_is-opened');
+// Открытие Popup по клику
+buttonProfileEdit.addEventListener('click', () => {
+  openPopup(popupTypeEdit)});
+
+profileAddButton.addEventListener('click', () => {
+  openPopup(popupTypeNewCard)});
+
+// buttonProfileEdit.addEventListener('click', function() {
+//   nameInput.value = page.querySelector('.profile__title').textContent;
+//   jobInput.value = page.querySelector('.profile__description').textContent;
+//   openPopup(popupTypeEdit);
 // });
-
-popupCloseButton.addEventListener('click', function(){
-  popup.classList.remove('popup_is-opened');
-}); 
---
+++ src/scripts/modal.js
@@ -0,0 +1,16 @@
+function openPopup(popup){
+  popup.classList.add('popup_is-opened');
+  popup.classList.add('popup_is-animated');
+};
+
+// // function closePopup(popup){
+// //   popup.classList.remove('popup_is-opened');
+// // }
+
+// // function consoleEsc(evt){
+// //   if(evt.key === 'Esc'){
+// //     closePopup;
+// //   }
+// // }
+
+export { openPopup };
